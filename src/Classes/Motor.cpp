d9e87242fc83829e75e843bece5fd6c42a02b48c
brunopinto <brunoapinto2004@gmail.com>
2023-10-31 13:11:12 -0300

    Consertando motor

--- src/Classes/Motor.cpp
+++ src/Classes/Motor.cpp
@@ -8,14 +8,12 @@
 // Diferença de temperatura relativa ao delay maximo (para a regressão linear)
 #define MAX_TEMP_DIFF 20.0
 
-Motor::Motor(int step,
-						 int dir,
-						 int led,
-						 int delay):
-	_step(step),
-	_dir(dir), 
-	_led(led), 
-	_delay(delay) {}
+Motor::Motor(int step, int dir, int led, int delay):
+		_step(step), _dir(dir), _led(led), _delay(delay) {
+
+		pinMode(dir, OUTPUT);
+		pinMode(step, OUTPUT);
+}
 
 void Motor::spin(int& motor_dir, int& motor_step) {
 	//Se o delay for maior que o mínimo, o motor acionará
@@ -35,7 +33,10 @@ void Motor::spin(int& motor_dir, int& motor_step) {
 }
 
 // THEO: Acredito que não seja necessária essa função, podemos acender o led no spin
-/* void Motor::led (int LED, int& _step){
+// BRUNO: Ao colocar o led no spin, achoq vai contra os principios da OO, pois as funções tem que 
+// que ser exclusivas, devem realizar apenas 1 tarefa
+
+/* void Motor::led (int LED){
 
 	if (_step == 0){
 		digitalWrite(LED, LOW);
