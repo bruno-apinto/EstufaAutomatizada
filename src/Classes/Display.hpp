0d43cdd63e583877d76478f79e49a2f102ae247b
rob1913r <148152111+rob1913r@users.noreply.github.com>
2023-11-01 00:01:07 -0300

    Update Display.hpp

--- src/Classes/Display.hpp
+++ src/Classes/Display.hpp
@@ -1,48 +1,37 @@
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
 
-#include "Adafruit_ILI9341.h"
+#include <Adafruit_ILI9341.h>
 
 class Display{
 public:
-	/**
-	* @brief Construtor do display
-	*/
-	Display(int tft_cs, int tft_dc, int current_temp,
-				 int select, int down, int up);
+	/// @brief Inicializa o display
+	Display(int tft_CS, int tft_DC, int current_temp, int select, int down, int up);
 
-	/**
-	* @brief Tela principal
-	* Mostra a temperatura atual e a alvo, o cursor fica fixo na opção
-	* de ir para a tela de seleção
-	*/
+	/// @brief Tela inicial, mostra a temperatura atual e a alvo
 	void main_screen();
 
-	/**
-	* @brief Tela de seleção da temperatura alvo
-	*/
-	void select_screen();	
+	/// @brief Tela de seleção da temperatura alvo
+	void select_screen();
 
-	/**
-	* @brief Define a tela a ser mostrada baseado na variavel tela
-	*/
+	/// @brief Atualiza o valor da temperatura atual
 	void update();
 
-	/**
-	 * @brief Retorna o valor da temperatura alvo
-	 * 
-	 * @return _target_temp 
-	 */
+	/// @return A tela atual
+	int get_tela();
+
+	/// @return A temperatura alvo
 	int get_target();
 
 private:
-	Adafruit_ILI9341 _tft; /// controlador do display
-	int _target_temp; /// temperatura alvo
-	int _tela; /// tela atual
+	Adafruit_ILI9341 _tft; // controlador do display
 
-	int _select; /// botao de seleção
-	int _down; /// botao de ir para baixo
-	int _up; /// botao de ir para cima
+	int _target_temp; // temperatura alvo
+	int _tela; // tela atual
+
+	int _select; // botao de seleção
+	int _down; // botao de ir para baixo
+	int _up; // botao de ir para cima
 };
 
 #endif //DISPLAY_HPP
